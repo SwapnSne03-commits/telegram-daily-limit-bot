52d33ce53895b178e0a4c7b4f77cb1f77dae24ba
SwapnSne03-commits <somsneha03@gmail.com>
2026-02-17 15:06:18 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,15 @@
+import os
+from pymongo import MongoClient
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+
+db = client["telegram_limit_bot"]
+
+groups_col = db["groups"]
+users_col = db["users"]
+admins_col = db["stats_admins"]
+force_config_col = db["force_config"]
+force_channels_col = db["force_channels"]
+force_verified_col = db["force_verified"]
